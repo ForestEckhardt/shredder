075e21aae87637320ab320cb9e97156c1b370266
Forest Eckhardt <forest.eckhardt@docker.com>
2025-10-16 15:43:06 -0400

    Adds -repo -org and -dry-run flags to allow for better filtering

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"os/exec"
 	"regexp"
@@ -13,7 +14,32 @@ import (
 	"github.com/google/go-github/v76/github"
 )
 
+// Custom type to collect multiple flags into a slice
+type stringSlice []string
+
+func (s *stringSlice) String() string {
+	return strings.Join(*s, ", ")
+}
+
+func (s *stringSlice) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+// If true the notification is discarded from the deletion pool
+type FilterFunc func(*github.Notification) bool
+
 func main() {
+	var repos stringSlice
+	var orgs stringSlice
+	var dryRun bool
+
+	flag.Var(&repos, "repo", "Specify a repository in the <owner>/<repository> format that should be skipped during deletion")
+	flag.Var(&orgs, "org", "Specify a organization that should be skipped during deletion")
+	flag.BoolVar(&dryRun, "dry-run", false, "Runs command and outputs notifications that would be deleted")
+
+	flag.Parse()
+
 	token, err := exec.Command("gh", "auth", "token").Output()
 	if err != nil {
 		log.Fatal(err)
@@ -44,10 +70,19 @@ func main() {
 	filteredNotificationsChan := make(chan *github.Notification, len(allNotifications))
 
 	var wg sync.WaitGroup
+	var filters []FilterFunc
+
+	if len(repos) > 0 {
+		filters = append(filters, createRepositoryFilter(repos))
+	}
+
+	if len(orgs) > 0 {
+		filters = append(filters, createOrganizationFilter(orgs))
+	}
 
 	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
-		go filter(allNotificationsChan, filteredNotificationsChan, ctx, client, &wg)
+		go filter(allNotificationsChan, filteredNotificationsChan, ctx, client, filters, &wg)
 	}
 
 	for _, notification := range allNotifications {
@@ -60,7 +95,7 @@ func main() {
 
 	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
-		go remove(filteredNotificationsChan, ctx, client, &wg)
+		go remove(filteredNotificationsChan, ctx, client, dryRun, &wg)
 	}
 
 	close(filteredNotificationsChan)
@@ -68,11 +103,20 @@ func main() {
 	wg.Wait()
 }
 
-func filter(allNotifications chan *github.Notification, filteredNotifications chan *github.Notification, ctx context.Context, client *github.Client, wg *sync.WaitGroup) {
+func filter(allNotifications chan *github.Notification, filteredNotifications chan *github.Notification, ctx context.Context, client *github.Client, filters []FilterFunc, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for notification := range allNotifications {
 		if notification.GetSubject().GetType() != "PullRequest" {
+			continue
+		}
+
+		var skip bool
+		for _, f := range filters {
+			skip = skip || f(notification)
+		}
+
+		if skip {
 			continue
 		}
 
@@ -101,22 +145,46 @@ func filter(allNotifications chan *github.Notification, filteredNotifications ch
 	}
 }
 
-func remove(removeNotifications chan *github.Notification, ctx context.Context, client *github.Client, wg *sync.WaitGroup) {
+func remove(removeNotifications chan *github.Notification, ctx context.Context, client *github.Client, dryRun bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for notification := range removeNotifications {
-		notificationID, err := strconv.ParseInt(notification.GetID(), 0, 64)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
+		if !dryRun {
+			notificationID, err := strconv.ParseInt(notification.GetID(), 0, 64)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
 
-		_, err = client.Activity.MarkThreadDone(ctx, notificationID)
-		if err != nil {
-			log.Printf("failed to mark notification %s as done %v", notification.GetID(), err)
-			continue
+			_, err = client.Activity.MarkThreadDone(ctx, notificationID)
+			if err != nil {
+				log.Printf("failed to mark notification %s as done %v", notification.GetID(), err)
+				continue
+			}
 		}
 
 		log.Printf("Removed notification for %s\n", notification.GetSubject().GetURL())
+	}
+}
+
+func createRepositoryFilter(repos []string) FilterFunc {
+	return func(notification *github.Notification) bool {
+		for _, r := range repos {
+			if notification.GetRepository().GetFullName() == r {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func createOrganizationFilter(orgs []string) FilterFunc {
+	return func(notification *github.Notification) bool {
+		for _, o := range orgs {
+			if notification.GetRepository().GetOwner().GetLogin() == o {
+				return true
+			}
+		}
+		return false
 	}
 }
